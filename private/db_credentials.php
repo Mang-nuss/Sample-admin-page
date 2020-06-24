c4094dc9204e0d4be7cb82e0af523cb2ecb98dbb
Magnus Johansson <johansson.magnus.86@gmail.com>
2020-06-24 11:12:09 +0200

    adjusting db credentials

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,13 +1,8 @@
 <?php
 //echo " - in db_credentials! - ";
-define("DB_SERVER", "127.0.0.1"); //127.0.0.1 or localhost. Solution found at stackoverflow.
+define("DB_SERVER", "<may_be_localhost>"); //127.0.0.1 or localhost. Solution found at stackoverflow.
 define("DB_USER", 'root');
-define("DB_PASSWORD", "sungam45962");
+define("DB_PASSWORD", "<your_password>"); #"sungam45962"
 define("DB_NAME", "ADMINPAGES");
-
-// define('DB_NAME','zgxjwvbb_npdb');
-// define('DB_USER','zgxjwvbb_mj');
-// define('DB_PASSWORD','@vNcA0oR1Df36.');
-// define('DB_HOST','localhost');
 ?>
 
